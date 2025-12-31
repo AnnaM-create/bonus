1b9ec964dcdbb8519c356244b9209f0bf0d32e0f
Anna <foxy2ears@gmail.com>
2025-12-31 14:18:25 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 15000;
+        int rublesPerMile = 20;
+        int bonusMiles = ticketPrice / rublesPerMile;
+        System.out.println("Начислено бонусных миль: " + bonusMiles);
+    }
+}
